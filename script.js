4401502180789cf620b56ed8420205007822458d
Jacob Wigent <jwigent18@gmail.com>
2023-09-19 22:20:23 -0400

    Redo Site

--- script.js
+++ script.js
@@ -11,15 +11,6 @@ const navLinks = document.querySelectorAll('.navbar ul li');
 changeTitle();
 setInterval(changeTitle, titleInterval);
 
-
-addEventListener('scroll', e => {
-    if (window.scrollY > 100) {
-        navbar.classList.add("drop-shadow") // Add drop shadow to navbar
-      } else {
-        navbar.classList.remove("drop-shadow") // Remove drop shadow from navbar
-      }
-})
-
 var typingEffect = new Typed(".multi-text",{
     strings : ["Game Designer","Web Developer","Electronics Hobbyist"],
     loop : true,
@@ -32,11 +23,6 @@ function changeTitle() {
     document.title = "Jacob Wigent - " + titles[currentTitleIndex%titles.length];
     currentTitleIndex++;
 }
-
-function lerp (start, end, amt){
-    return (1-amt)*start+amt*end
-}
-
 
 function navSlide() {
     //Toggle nav
